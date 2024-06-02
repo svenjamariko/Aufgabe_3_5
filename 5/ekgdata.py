8de0b65a5ea772c904e55cffe43bf85f3e782019
felithie <felicitasthierbach@gmail.com>
2024-06-02 16:09:45 +0200

    everything (Funktioniert, bis auf das es keine EKG Daten findet)

--- 5/ekgdata.py
+++ 5/ekgdata.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
+import streamlit as st
 
 # %% Objekt-Welt
 
--- 5/main.py
+++ 5/main.py
@@ -2,8 +2,12 @@ import streamlit as st
 import read_person_data
 import ekgdata
 import matplotlib.pyplot as plt
+from PIL import Image
 
-#%% Zu Beginn
+def callback():
+    print(f"The user has changed to{st.session_state.aktuelle_versuchsperson} and {st.session_state.aktuelles_ekg}")
+
+# Zu Beginn
 
 # Lade alle Personen
 person_names = read_person_data.get_person_list(read_person_data.load_person_data())
@@ -17,9 +21,11 @@ if 'aktuelle_versuchsperson' not in st.session_state:
 if 'picture_path' not in st.session_state:
     st.session_state.picture_path = 'data/pictures/none.jpg'
 
-## TODO: Session State für Pfad zu EKG Daten 
+## Session State für Pfad zu EKG Daten 
+if 'ekg_path' not in st.session_state:
+    st.session_state.ekg_path = ''
 
-#%% Design des Dashboards
+# Design des Dashboards
 
 # Schreibe die Überschrift
 st.write("# EKG APP")
@@ -28,40 +34,57 @@ st.write("## Versuchsperson auswählen")
 # Auswahlbox, wenn Personen anzulegen sind
 st.session_state.aktuelle_versuchsperson = st.selectbox(
     'Versuchsperson',
-    options = person_names, key="sbVersuchsperson")
+    options = person_names, key="select_versuchsperson")
 
 # Name der Versuchsperson
-st.write("Der Name ist: ", st.session_state.aktuelle_versuchsperson) 
+st.write("Der Name ist: ", st.session_state.aktuelle_versuchsperson)
+
+# Date input slot
+current_person_dict = read_person_data.find_person_data_by_name(st.session_state.aktuelle_versuchsperson)
+
+ekg_data = current_person_dict["ekg_tests"]
+ekg_datum = [ekg["date"] for ekg in ekg_data]
+
+st.write("## Datum auswählen")
+st.session_state.aktuelles_ekg = st.selectbox(
+    'EKG-Datum',
+    options = ekg_datum, key="sbEKGDatum",
+    on_change=callback)
+ekg_data = [ekg for ekg in ekg_data if ekg["date"] == st.session_state.aktuelles_ekg][0]
+
+
+st.write("Das ausgewählte Datum ist:", ekg_data)
 
 # TODO: Weitere Daten wie Geburtsdatum etc. schön anzeigen
 
 # Nachdem eine Versuchsperson ausgewählt wurde, die auch in der Datenbank ist
 # Finde den Pfad zur Bilddatei
 if st.session_state.aktuelle_versuchsperson in person_names:
-    st.session_state.picture_path = read_person_data.find_person_data_by_name(st.session_state.aktuelle_versuchsperson)["picture_path"]
-    # st.write("Der Pfad ist: ", st.session_state.picture_path) 
+    person_data = read_person_data.find_person_data_by_name(st.session_state.aktuelle_versuchsperson)
+    st.session_state.picture_path = person_data.get("picture_path", 'data/pictures/none.jpg')
+    st.session_state.ekg_path = person_data.get("ekg_path", '')  # Use .get to handle missing keys
 
-#%% Bild anzeigen
-
-from PIL import Image
+# Bild anzeigen
 image = Image.open(st.session_state.picture_path)
 st.image(image, caption=st.session_state.aktuelle_versuchsperson)
 
-#% Öffne EKG-Daten
+# Öffne EKG-Daten
 # TODO: Für eine Person gibt es ggf. mehrere EKG-Daten. Diese müssen über den Pfad ausgewählt werden können
 # Vergleiche Bild und Person
-current_egk_data = ekgdata.EKGdata(r"data\ekg_data\01_Ruhe_short.txt")
+if st.session_state.ekg_path:
+    current_ekg_data = ekgdata.EKGdata(st.session_state.ekg_path)
 
-#%% EKG-Daten als Matplotlib Plot anzeigen
-# Nachdem die EKG, Daten geladen wurden
-# Erstelle den Plot als Attribut des Objektes
-current_egk_data.plot_time_series()
-# Zeige den Plot an
-st.pyplot(fig=current_egk_data.fig)
+    # EKG-Daten als Matplotlib Plot anzeigen
+    # Nachdem die EKG, Daten geladen wurden
+    # Erstelle den Plot als Attribut des Objektes
+    current_ekg_data.plot_time_series()
+    # Zeige den Plot an
+    st.pyplot(fig=current_ekg_data.fig)
 
-
-# %% Herzrate bestimmen
-# Schätze die Herzrate 
-current_egk_data.estimate_hr()
-# Zeige die Herzrate an
-st.write("Herzrate ist: ", int(current_egk_data.heat_rate)) 
+    # Herzrate bestimmen
+    # Schätze die Herzrate 
+    current_ekg_data.estimate_hr()
+    # Zeige die Herzrate an
+    st.write("Herzrate ist: ", int(current_ekg_data.heat_rate))
+else:
+    st.write("Keine EKG-Daten vorhanden.")
